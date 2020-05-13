9f55f9a4938dc43a2603a8d0f910d36290ec7ba2
Carlos Echeverria <echeverriadev@gmail.com>
2020-05-13 19:00:41 -0400

    Avatar component added

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar page'),
+        actions: <Widget>[ 
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar( 
+              backgroundImage: NetworkImage('https://i0.pngocean.com/files/171/434/384/clark-kent-superman-man-of-steel-batman-comic-book-henry-cavill-superman.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar( 
+              child: Text('CG'),
+              backgroundColor: Colors.red,
+            ),
+          ),
+          
+        ],
+      ),
+      body: Center(
+        child: FadeInImage( 
+          image: NetworkImage('https://i0.pngocean.com/files/171/434/384/clark-kent-superman-man-of-steel-batman-comic-book-henry-cavill-superman.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 100),
+        ),
       ),
     );
   }

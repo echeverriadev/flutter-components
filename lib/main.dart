4dac8307cf8b1346c7bbf4c8ad668fd789153325
Carlos Echeverria <echeverriadev@gmail.com>
2020-05-13 14:05:06 -0400

    components list reading json file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:components_fluter/src/home_temp.dart';
+import 'package:components_fluter/src/home_page.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false, // para quitar el liston rojo de debug
-      home: HomePageTemp()
+      home: HomePage()
     );
   }
 }
